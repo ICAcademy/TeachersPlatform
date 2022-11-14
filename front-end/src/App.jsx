95f17644b522a7683b57a59db894e3158d0a6a56
Tolik9924 <sakurasoft9924@gmail.com>
2022-11-14 16:55:40 +0200

    made theme style file

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+
+// theme style
+import theme from './styles/customTheme';
+import { Button } from '@mui/material';
 
 const App = () => {
   return (
-    <div>Teachers platform</div>
+    <ThemeProvider theme={theme}>
+      <div>Teachers platform</div>
+      <Button
+        onClick={() => console.log('you clicked me')}
+        color="primary" 
+        variant="contained"
+        >
+        Button
+      </Button>
+    </ThemeProvider>
   );
 };
 
