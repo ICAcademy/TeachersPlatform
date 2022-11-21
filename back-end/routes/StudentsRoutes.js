d7cf3a020f48a3e1f423f5e6dfe0c635d979a056
Pavlo Kataranchuk <pavel83838300@gmail.com>
2022-11-21 11:46:44 +0200

    removed log & fixed material routes

--- back-end/routes/StudentsRoutes.js
+++ back-end/routes/StudentsRoutes.js
@@ -10,9 +10,6 @@ const {
 
 const router = express.Router();
 
-// router.route('/').get(getAllStudents).post(createStudents);
-// router.route('/:id').get(getStudentsById).put(updateStudents).delete(deleteStudents);
-
 router.get('/get-all-students', getAllStudents);
 router.post('/create-students', createStudents);
 router.get('/get-all-students/:id', getStudentsById);
