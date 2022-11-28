5f650e34ccccfc5ffdd70a9c1d1a1b1770a6f5f3
Pavlo Kataranchuk <pavel83838300@gmail.com>
2022-11-28 15:38:45 +0200

    fixed routes

--- back-end/routes/StudentsRoutes.js
+++ back-end/routes/StudentsRoutes.js
@@ -10,10 +10,10 @@ const {
 
 const router = express.Router();
 
-router.get('/get-all-students', getAllStudents);
-router.post('/create-students', createStudents);
-router.get('/get-all-students/:id', getStudentsById);
-router.put('/update-students/:id', updateStudents);
-router.delete('/delete-students/:id', deleteStudents);
+router.get('/', getAllStudents);
+router.post('/', createStudents);
+router.get('/:id', getStudentsById);
+router.put('/:id', updateStudents);
+router.delete('/:id', deleteStudents);
 
 module.exports = router;
