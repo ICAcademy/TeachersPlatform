6fa6aa5d0a91a510085ce0ab1f1048a948a2b6f5
Pavlo Kataranchuk <pavel83838300@gmail.com>
2022-11-29 17:23:40 +0200

    finished configuration for upload photos to Firebase

--- back-end/app.js
+++ back-end/app.js
@@ -11,14 +11,10 @@ const materialRouter = require('./routes/MaterialRoutes');
 const materialLevelsRouter = require('./routes/MaterialLevelsRoutes');
 const questionRouter = require('./routes/Questions');
 const studentsRouter = require('./routes/StudentsRoutes');
-const teacherRouter = require('./routes/TeacherRoutes');
 const authUser = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-<<<<<<< HEAD
-const authUser = require('./routes/auth');
 
 const upload = require('./Firebase');
 
@@ -26,13 +22,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
-=======
-const cors = require('cors');
 
-app.use(cors());
-app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
->>>>>>> 2a89e5c13ecf92781beb910e6b721b5905bdbfb4
 
 app.use('/auth', authUser);
 app.use('/api/materials', materialRouter);
