1f98bcd7182ca1320c0c31190fd05bfd1fb90d9e
nadiakohli <andruknadia@gmail.com>
2022-12-05 16:22:19 +0200

    delete unused files

--- back-end/exceptions/StatusError.js
+++
@@ -1,7 +0,0 @@
-class StatusError extends Error {
-  constructor(status, message) {
-    super(message);
-    this.status = status;
-  }
-}
-module.exports = StatusError;
--- back-end/helpers/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const StatusError = require('../exceptions/StatusError');
-
-const errorHandler = (res, error, status = null) => {
-  if (error instanceof StatusError)
-    return res.status(error.status).json({ status: error.status, message: error.message });
-  res.status(status || 500).json({ status: status || 500, message: error.message });
-};
-
-module.exports = errorHandler;
