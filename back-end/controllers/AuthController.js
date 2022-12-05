28440f5e4235595af17c51fc34037e5f7ef161e6
nadiakohli <andruknadia@gmail.com>
2022-12-05 19:26:25 +0200

    destructure and change status of creating user

--- back-end/controllers/AuthController.js
+++ back-end/controllers/AuthController.js
@@ -18,12 +18,11 @@ const createRoleForUser = async (role, data) => {
   return await createStudent(data);
 };
 
-exports.createUser = async (req, res) => {
+exports.createUser = async ({ body }, res) => {
   try {
-    const { email } = req.body;
-    const { error } = registerValidation(req.body);
+    const { error } = registerValidation(body);
 
-    const user = await findByEmail(email);
+    const user = await findByEmail(body.email);
 
     if (user) {
       return res.status(400).json({ message: 'User with that email address already exists' });
@@ -32,18 +31,17 @@ exports.createUser = async (req, res) => {
       return res.status(400).send(error.details[0].message);
     }
 
-    await register(req.body);
-    await createRoleForUser(req.body.role, req.body);
-    res.status(200).json({ message: 'User was successfully created!' });
+    register(body);
+    await createRoleForUser(body.role, body);
+    res.status(201).json({ message: 'User was successfully created!' });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-exports.loginUser = async (req, res) => {
+exports.loginUser = async ({ body }, res) => {
   try {
-    const data = req.body;
-    const token = await login(data);
+    const token = await login(body);
 
     if (!token) {
       return res.status(400).json({ message: 'User was not found!' });
