01d2ec4989255b036c21879da30436c0ef8d478c
Pavlo Kataranchuk <pavel83838300@gmail.com>
2022-12-05 16:36:09 +0200

    filtering

--- front-end/src/pages/Materials/Materials.jsx
+++ front-end/src/pages/Materials/Materials.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash.debounce';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 //Services
-import { getUnitsByLevel } from 'services/MaterialsService/MaterialsService';
+import { getUnitsByLevel, getMaterialsByUnit } from 'services/MaterialsService/MaterialsService';
 
 //Components
 import Levels from 'components/Materials/Levels/Levels';
@@ -10,12 +13,14 @@ import Loader from 'components/common/Loader/Loader';
 
 //Styles
 import styles from './Materials.module.scss';
+import { Button } from '@mui/material';
 
 const Materials = () => {
   const [selectedLevel, setSelectedLevel] = useState('beginner');
   const [unitsByLevel, setUnitsByLevel] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState('');
+  const [searchByUnit, setSearchByUnit] = useState([]);
 
   const unitsByLevelData = async (level) => {
     try {
@@ -28,12 +33,26 @@ const Materials = () => {
     }
   };
 
+  const MaterialsByUnit = async () => {
+    try {
+      setIsLoading(true);
+      const data = await getMaterialsByUnit(search);
+      setSearchByUnit(data);
+      console.log(data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const changeLevelHandler = (level) => {
     setSelectedLevel(level);
     unitsByLevelData(level);
   };
 
   const handleInput = (e) => setSearch(e.target.value);
+
+  const debouncedHandleInput = useCallback(debounce(handleInput, 300), []);
 
   useEffect(() => {
     unitsByLevelData(selectedLevel);
@@ -45,9 +64,12 @@ const Materials = () => {
         className={styles.materialsSearch}
         type='text'
         placeholder='Enter here to find a lesson'
-        value={search}
+        defaultValue={search}
         onChange={handleInput}
       />
+      <Button className={styles.searchIcon} onClick={MaterialsByUnit}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </Button>
       <Levels selectedLevel={selectedLevel} onChangeLevel={changeLevelHandler} />
       {isLoading ? <Loader /> : <Units materials={unitsByLevel} />}
     </div>
