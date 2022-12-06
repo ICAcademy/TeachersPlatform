d3082237fa502743f8933785ad37ae7976bdb861
nadiakohli <andruknadia@gmail.com>
2022-12-06 15:51:06 +0200

    rewrite body to req.body in AuthController

--- back-end/controllers/AuthController.js
+++ back-end/controllers/AuthController.js
@@ -18,11 +18,11 @@ const createRoleForUser = async (role, data) => {
   return await createStudent(data);
 };
 
-exports.createUser = async ({ body }, res) => {
+exports.createUser = async (req, res) => {
   try {
-    const { error } = registerValidation(body);
+    const { error } = registerValidation(req.body);
 
-    const user = await findByEmail(body.email);
+    const user = await findByEmail(req.body.email);
 
     if (user) {
       return res.status(400).json({ message: 'User with that email address already exists' });
@@ -31,17 +31,17 @@ exports.createUser = async ({ body }, res) => {
       return res.status(400).send(error.details[0].message);
     }
 
-    register(body);
-    await createRoleForUser(body.role, body);
+    register(req.body);
+    await createRoleForUser(req.body.role, req.body);
     res.status(201).json({ message: 'User was successfully created!' });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-exports.loginUser = async ({ body }, res) => {
+exports.loginUser = async (req, res) => {
   try {
-    const token = await login(body);
+    const token = await login(req.body);
 
     if (!token) {
       return res.status(400).json({ message: 'User was not found!' });
