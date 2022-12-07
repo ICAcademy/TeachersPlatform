aa76746e02b0edc4349e2c83a80d5ea969cca91d
nadiakohli <andruknadia@gmail.com>
2022-12-07 10:36:13 +0200

    change path of api /auth/me to /api/me

--- front-end/src/services/userService.js
+++ front-end/src/services/userService.js
@@ -3,7 +3,7 @@ import API from 'API';
 export const userService = {
   getUser: async () => {
     try {
-      const user = await API.get('/auth/me');
+      const user = await API.get('/api/me');
       return user;
     } catch (e) {
       console.log(e);
