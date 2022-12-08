017e9783eaf3635173b4e592f1f9ef8fa1cc829e
nadiakohli <andruknadia@gmail.com>
2022-12-08 16:31:46 +0200

    add show password in login form and install @mui/icons-material

--- front-end/src/components/LoginForm/index.jsx
+++ front-end/src/components/LoginForm/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useMemo, useContext } from 'react';
-import { FormControl, TextField, Box, Button, InputAdornment } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { nanoid } from 'nanoid';
+
+// MUI library
+import { FormControl, TextField, Box, Button, InputAdornment, IconButton } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
-import { nanoid } from 'nanoid';
-import { Link, useNavigate } from 'react-router-dom';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 // Services
 import { authService } from 'services/authService';
@@ -22,12 +25,24 @@ const LoginForm = () => {
     password: '',
   });
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const [isError, setIsError] = useState(false);
 
   const history = useNavigate();
 
-  const handleChange = ({ target: { name, value } }) => {
+  const handleMouseDownPassword = (e) => e.preventDefault();
+
+  const handleEmailChange = ({ target: { name, value } }) => {
     setData({ ...data, [name]: value });
+  };
+
+  const handlePasswordChange = (prop) => (e) => {
+    setData({ ...data, [prop]: e.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
   const isDisabledBtn = useMemo(
@@ -64,12 +79,12 @@ const LoginForm = () => {
         <Box>
           <FormControl sx={{ width: '400px' }}>
             <TextField
-              error={false}
               id={`input-with-icon-textfield ${nanoid(5)}`}
               type='email'
               name='email'
               value={data.email}
-              onChange={(e) => handleChange(e)}
+              onChange={(e) => handleEmailChange(e)}
+              error={isError}
               placeholder='Email'
               color='purple'
               size='small'
@@ -83,12 +98,12 @@ const LoginForm = () => {
               }}
             />
             <TextField
-              id={`input-with-icon-textfield ${nanoid(5)}`}
-              type='password'
+              id={`standard-adornment-password ${nanoid(5)}`}
+              type={showPassword ? 'text' : 'password'}
               name='password'
-              error={false}
               value={data.password}
-              onChange={(e) => handleChange(e)}
+              onChange={handlePasswordChange('password')}
+              error={isError}
               placeholder='Password'
               color='purple'
               size='small'
@@ -96,6 +111,22 @@ const LoginForm = () => {
                 startAdornment: (
                   <InputAdornment position='start'>
                     <FontAwesomeIcon icon={faLock} fill='#a1a4b5' width={12} height={12} />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge='end'
+                    >
+                      {showPassword ? (
+                        <VisibilityOff sx={{ width: '20px', height: '20px' }} />
+                      ) : (
+                        <Visibility sx={{ width: '20px', height: '20px' }} />
+                      )}
+                    </IconButton>
                   </InputAdornment>
                 ),
               }}
