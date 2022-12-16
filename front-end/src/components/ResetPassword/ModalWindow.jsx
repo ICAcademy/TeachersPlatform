62441770d9b671b6810f4eea9c1f22a6bc0a1284
Pavlo Kataranchuk <pavel83838300@gmail.com>
2022-12-16 17:05:23 +0200

    added styles

--- front-end/src/components/ResetPassword/ModalWindow.jsx
+++ front-end/src/components/ResetPassword/ModalWindow.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import PropTypes, { bool } from 'prop-types';
-import { Modal, Box, TextField, Button, FormControl } from '@mui/material';
+import {
+  Modal,
+  Box,
+  TextField,
+  Button,
+  FormControl,
+  InputAdornment,
+  IconButton,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const style = {
   position: 'absolute',
@@ -18,39 +27,101 @@ const style = {
 };
 
 const ModalWindow = ({ open, handleClose }) => {
-  const [password, setPassword] = useState('');
-  const handlePassword = (e) => setPassword(e.target.value);
+  const [showPassword, setShowPassword] = useState(false);
+  const handleMouseDownPassword = (e) => e.preventDefault();
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <Modal open={open} onClose={handleClose}>
       <Box sx={style}>
-        <FormControl sx={{ width: '100%', marginBottom: '20px' }}>
+        <FormControl sx={{ width: '100%' }}>
           <TextField
-            margin='dense'
+            margin='normal'
             variant='outlined'
             size='small'
             label='Current password'
-            defaultValue={password}
-            onChange={handlePassword}
+            type={showPassword ? 'text' : 'password'}
+            name='password'
             InputLabelProps={{ shrink: true }}
+            InputProps={{
+              startAdornment: <InputAdornment position='start'></InputAdornment>,
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {showPassword ? (
+                      <VisibilityOff sx={{ width: '20px', height: '20px' }} />
+                    ) : (
+                      <Visibility sx={{ width: '20px', height: '20px' }} />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
           <TextField
-            margin='dense'
+            margin='normal'
             variant='outlined'
             size='small'
             label='New password'
-            defaultValue={password}
-            onChange={handlePassword}
+            type={showPassword ? 'text' : 'password'}
+            name='password'
             InputLabelProps={{ shrink: true }}
+            InputProps={{
+              startAdornment: <InputAdornment position='start'></InputAdornment>,
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {showPassword ? (
+                      <VisibilityOff sx={{ width: '20px', height: '20px' }} />
+                    ) : (
+                      <Visibility sx={{ width: '20px', height: '20px' }} />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
           <TextField
-            margin='dense'
+            sx={{ marginBottom: '20px' }}
+            margin='normal'
             variant='outlined'
             size='small'
             label='New password again'
-            defaultValue={password}
-            onChange={handlePassword}
+            type={showPassword ? 'text' : 'password'}
+            name='password'
             InputLabelProps={{ shrink: true }}
+            InputProps={{
+              startAdornment: <InputAdornment position='start'></InputAdornment>,
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {showPassword ? (
+                      <VisibilityOff sx={{ width: '20px', height: '20px' }} />
+                    ) : (
+                      <Visibility sx={{ width: '20px', height: '20px' }} />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
           <Box
             sx={{
