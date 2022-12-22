e0869b367c3afc49380ad69252b0535071d95a36
Tolik9924 <sakurasoft9924@gmail.com>
2022-12-22 14:32:23 +0200

    fixed some issues

--- front-end/src/components/Profile/TeacherInfo/AgePreferences/AgePreferences.jsx
+++ front-end/src/components/Profile/TeacherInfo/AgePreferences/AgePreferences.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AgePreferences = () => {
-  return <div>AgePreferences</div>;
+// components
+import { Input } from '@mui/material';
+
+// styles
+import styles from './AgePreferences.module.scss';
+
+const AgePreferences = ({ minAge, setMinAge, maxAge, setMaxAge }) => {
+  const changeMinAge = (event) => {
+    setMinAge(event.target.value);
+  };
+
+  const changeMaxAge = (event) => {
+    setMaxAge(event.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>Age Preferences</div>
+      <div className={styles.minAgeContainer}>
+        <Input value={minAge} onChange={changeMinAge} type='number' />
+      </div>
+      <span>-</span>
+      <div>
+        <Input value={maxAge} onChange={changeMaxAge} type='number' />
+      </div>
+    </div>
+  );
+};
+
+AgePreferences.propTypes = {
+  minAge: PropTypes.number,
+  setMinAge: PropTypes.func,
+  maxAge: PropTypes.number,
+  setMaxAge: PropTypes.func,
 };
 
 export default AgePreferences;
