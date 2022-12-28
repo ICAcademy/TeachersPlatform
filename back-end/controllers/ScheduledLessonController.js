01338463e7a1bbd2fc4097f28aabecea873f412f
DeLettenhove <deletten@gmail.com>
2022-12-28 13:44:05 +0200

    Add funct that check if selected time is free, add query param to func that reurn all lessons to get lessons for specific date

--- back-end/controllers/ScheduledLessonController.js
+++ back-end/controllers/ScheduledLessonController.js
@@ -4,16 +4,19 @@ const {
   scheduleLesson,
   updateLesson,
   deleteLesson,
+  checkTime,
 } = require('../services/ScheduledLessonService');
 
 const getAllScheduledLessons = async (req, res) => {
   try {
-    const lessons = await getAllLessons();
+    const { date } = req.query;
+    const lessons = await getAllLessons(date);
     res.status(200).json(lessons);
   } catch (error) {
     res.status(400).json(error);
   }
 };
+
 const getScheduledLesson = async (req, res) => {
   try {
     const { id } = req.params;
@@ -23,14 +26,23 @@ const getScheduledLesson = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
 const createScheduledLesson = async (req, res) => {
   try {
+    const isTimeAlreadyTaken = await checkTime(req.body.date);
+
+    if (isTimeAlreadyTaken) {
+      return res.status(400).json({ msg: 'There is another lesson scheduled for this time' });
+    }
+
     const lesson = await scheduleLesson(req.body);
+
     res.status(200).json(lesson);
   } catch (error) {
     res.status(400).json(error);
   }
 };
+
 const updateScheduledLesson = async (req, res) => {
   try {
     const { id } = req.params;
@@ -40,6 +52,7 @@ const updateScheduledLesson = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
 const deleteScheduledLesson = async (req, res) => {
   try {
     const { id } = req.params;
--- back-end/services/ScheduledLessonService.js
+++ back-end/services/ScheduledLessonService.js
@@ -1,10 +1,30 @@
 const ScheduledLesson = require('../models/ScheduledLesson');
 
-const getAllLessons = async () => await ScheduledLesson.find({});
+const getAllLessons = async (date) => {
+  if (date) {
+    return await ScheduledLesson.find({ date: { $regex: date, $options: 'i' } });
+  }
+  return await ScheduledLesson.find({});
+};
+
 const getLessonById = async (id) => await ScheduledLesson.findById(id);
+
 const scheduleLesson = async (lesson) => await ScheduledLesson.create(lesson);
+
 const updateLesson = async (id, body) =>
   await ScheduledLesson.findByIdAndUpdate(id, body, { new: true, runValidators: true });
+
 const deleteLesson = async (id) => await ScheduledLesson.findByIdAndDelete(id);
 
-module.exports = { getAllLessons, getLessonById, scheduleLesson, updateLesson, deleteLesson };
+const checkTime = async (time) => {
+  return Boolean(await ScheduledLesson.findOne({ date: time }));
+};
+
+module.exports = {
+  getAllLessons,
+  getLessonById,
+  scheduleLesson,
+  updateLesson,
+  deleteLesson,
+  checkTime,
+};
