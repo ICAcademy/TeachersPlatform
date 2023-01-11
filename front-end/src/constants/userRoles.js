e034c91aacbd7820300e718d5ce14b232123bffd
Tolik9924 <sakurasoft9924@gmail.com>
2023-01-11 11:27:29 +0200

    user roles

--- front-end/src/constants/userRoles.js
+++ front-end/src/constants/userRoles.js
@@ -1,3 +1,3 @@
-export const TEACHER_ROLE = 'teacher';
-export const STUDENT_ROLE = 'student';
-export const ADMIN_ROLE = 'admin';
+export const TEACHER = 'teacher';
+export const STUDENT = 'student';
+export const ADMIN = 'admin';
