0a3b95812eab767ade43bf9191b540cbdd736297
sergeyevalex90 <sergeyevalex90@gmail.com>
2023-01-13 16:39:10 +0200

    fix: fixing socket url for frontend and backend depemding on dev or prod mode

--- front-end/src/services/socketService.js
+++ front-end/src/services/socketService.js
@@ -1,5 +1,10 @@
 import io from 'socket.io-client';
 
-const URL = 'https://teacher-platform.onrender.com';
+let URL;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  URL = 'http://localhost:5000';
+} else {
+  URL = 'https://teacher-platform.onrender.com';
+}
 
 export const socket = io.connect(URL);
