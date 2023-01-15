f26538fb69027ae05c20946ebbbf77a1f7d3f506
sergeyevalex90 <sergeyevalex90@gmail.com>
2023-01-15 13:14:15 +0200

    fix: approve the code in soket service on frontend

--- front-end/src/services/socketService.js
+++ front-end/src/services/socketService.js
@@ -1,10 +1,9 @@
 import io from 'socket.io-client';
 
-let URL;
+let URL = 'https://teacher-platform.onrender.com';
+
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   URL = 'http://localhost:5000';
-} else {
-  URL = 'https://teacher-platform.onrender.com';
 }
 
 export const socket = io.connect(URL);
