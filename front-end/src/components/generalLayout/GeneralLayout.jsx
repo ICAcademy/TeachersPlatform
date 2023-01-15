1c54e18b4d475a744e7dd14557dcfceaadd730c5
sergeyevalex90 <sergeyevalex90@gmail.com>
2023-01-15 13:08:34 +0200

    Merge branch 'develop' into feature/Add-approve-student-functionality

--- front-end/src/components/generalLayout/GeneralLayout.jsx
+++ front-end/src/components/generalLayout/GeneralLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './GeneralLayout.module.scss';
 import Sidebar from 'components/Sidebar/Sidebar';
 import { Outlet } from 'react-router';
+import MeetRoom from 'components/MeetRoom/MeetRoom';
 
 const GeneralLayout = () => {
   return (
     <>
       <Sidebar />
       <div className={styles.contentWrapper}>
+        <MeetRoom />
         <Outlet />
       </div>
     </>
--- front-end/src/routes/AppRouter.jsx
+++ front-end/src/routes/AppRouter.jsx
@@ -13,7 +13,6 @@ import Loader from 'components/common/Loader/Loader';
 import GeneralLayout from 'components/generalLayout/GeneralLayout';
 import GeneralInfo from 'components/Profile/GeneralInfo/GeneralInfo';
 import TeacherInfo from 'components/Profile/TeacherInfo/TeacherInfo';
-import MeetRoom from 'components/MeetRoom/MeetRoom';
 
 // Constants
 import { ADMIN_ROLE, STUDENT_ROLE, TEACHER_ROLE } from 'constants/userRoles';
@@ -47,7 +46,6 @@ const RouterWrapper = () => {
           element={
             <PrivateRoute>
               <GeneralLayout />
-              <MeetRoom />
             </PrivateRoute>
           }
         >
