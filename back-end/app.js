6eb6c5c06e2003211b8e74c38cca7bbcba72a8c6
Tolik9924 <sakurasoft9924@gmail.com>
2023-01-20 12:42:02 +0200

    resolve comments

--- back-end/app.js
+++ back-end/app.js
@@ -35,27 +35,18 @@ Sentry.init({
   release: process.env.SENTRY_PROJECT,
   dsn: process.env.SENTRY_DSN,
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
   ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
+
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
   res.statusCode = 500;
   res.end(res.sentry + '\n');
 });
@@ -87,7 +78,6 @@ async function main() {
     });
   } catch (err) {
     Sentry.captureException(err);
-    console.log('error', err);
   }
 }
 
