4e84bdf781e8979c26fa0a91fa28d259250aab4e
nadiakohli <andruknadia@gmail.com>
2023-02-03 14:59:40 +0200

    fix regex and validation in dictionary

--- back-end/helpers/dictionaryValidation.js
+++ back-end/helpers/dictionaryValidation.js
@@ -1,12 +1,17 @@
+/* eslint-disable quotes */
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-useless-escape */
 const Joi = require('joi');
 
 const schema = Joi.object({
   studentId: Joi.string().required(),
-  word: Joi.string().regex(new RegExp('^([a-zA-Z]{0,20})(?: |)([a-zA-Z]{0,20})$')).required(),
+  word: Joi.string().regex(new RegExp('^([a-zA-Z]{0,20})(?: |, |)([a-zA-Z]{0,20})$')).required(),
   translation: Joi.string()
-    .regex(new RegExp('^([А-ЩЬЮЯҐЄІЇа-щьюяґєії]{0,20})(?: |)([А-ЩЬЮЯҐЄІЇа-щьюяґєії]{0,20})$'))
+    .regex(
+      new RegExp(
+        "^([А-ЩЬЮЯҐЄІЇа-щьюяґєії']{0,20})(?: |, |-|)([А-ЩЬЮЯҐЄІЇа-щьюяґєії']{0,20})(?: |, |-|)([А-ЩЬЮЯҐЄІЇа-щьюяґєії']{0,20})$",
+      ),
+    )
     .required(),
 });
 
