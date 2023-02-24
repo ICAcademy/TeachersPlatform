f5f757e099ddf3a3369f2669825720bf7bd20163
sergeyevalex90 <sergeyevalex90@gmail.com>
2023-02-24 19:36:20 +0200

    feat: add new urls for deploy

--- front-end/src/API.js
+++ front-end/src/API.js
@@ -7,7 +7,7 @@ let modeUrl;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   modeUrl = 'http://localhost:5000';
 } else {
-  modeUrl = 'https://inter-school.onrender.com';
+  modeUrl = 'https://intercode-learning-school.netlify.app';
 }
 
 export const API_URL = modeUrl;
